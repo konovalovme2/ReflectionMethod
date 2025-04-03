bd71ecf3f096493451a38aa18e0aaa72a6c54130
konovalovme2 <Konovalovme2@gmail.com>
2025-04-03 15:33:58 +0600

    Добавил приведение к верхнетреугольной матрице

---
+++ src/main/java/com/examples/konovalov/App.java
@@ -0,0 +1,22 @@
+package com.examples.konovalov;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        double[][] Matrix = new double[][]{{2,2,1},{1,6,5},{2,1,1}};
+        ReflectionMethod newMatrix = new ReflectionMethod(Matrix);
+
+        double[][] A = newMatrix.findReversedMatrix();
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A.length; j++) {
+                System.out.println(A[i][j] + " ");
+            }
+            System.out.println("\n");
+        }
+
+        System.out.println( "Hello World!" );
+    }
+}
